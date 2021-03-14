fc54cc53795e1afa4ac552cdc623c7a9d8573a7b
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-14 20:38:46 -0300

    C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "";
+            Console.Write("Digite uma frase: ");
+            frase = Console.ReadLine();
+            Console.Clear();
+            frase = frase.Replace('r', 'l');
+            frase = frase.Replace('R', 'L');
+            Console.WriteLine("Como o cebolinha diria: "+frase);
+            Console.ReadKey();
+        }
+    }
+}
